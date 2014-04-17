1f29d0d002a639624cf98e27eb8a216ba88c00c5
Bill Rex <rexw3035@gmail.com>
2014-04-17 13:07:52 -0500

    Added imports/parameters for YAML files ala Symfony. Currently the import function is crude and just imports relative path files, no class parsing or anything. Parameters are currently parsed into the scope, but the scope is still global and no variable replacement is done yet.

--- src/Yaml.php
+++ src/Yaml.php
@@ -3,7 +3,7 @@
 namespace BurningDiode\Slim\Config;
 
 use Slim\Slim;
-use Symfony\Component\Yaml\Yaml as SymYaml;
+use Symfony\Component\Yaml\Yaml as YamlParser;
 
 class Yaml
 {
@@ -14,27 +14,43 @@ class Yaml
 	 */
 	protected static $instance = null;
 
+	protected static $slim = null;
+
+	protected static $parameters = array();
+
 	/**
 	 * addFile - Parse .yml file and add it to slim
 	 *
+	 * @param string $file
+	 * @param bool $reset (optional)
+	 *
 	 * @return void
 	 */
-	public function addFile($file)
+	public function addFile($file, $reset = true)
 	{
 		if (!file_exists($file) || !is_file($file)) {
 			throw new \Exception('The configuration file does not exist.');
 		} else {
-			$slim = Slim::getInstance();
-			$yaml = SymYaml::parse(file_get_contents($file));
+			if ($reset === true) {
+				self::$parameters = array();
+			}
 
-			foreach ($yaml as $key => $value) {
-				$slim->config($key, $value);
+			$content = YamlParser::parse(file_get_contents($file));
+
+			if ($content !== null) {
+				$content = self::parseImports($content, $file);
+
+				$content = self::parseParameters($content);
+
+				self::addConfig($content);
 			}
 		}
 	}
 
 	/**
 	 * addDirectory - Parse .yml files in a given directory
+	 *
+	 * @param string $directory
 	 *
 	 * @return void
 	 */
@@ -75,6 +91,43 @@ class Yaml
 	public static function _()
 	{
 		return self::getInstance();
+	}
+
+	protected function addConfig($content)
+	{
+		if (self::$slim === null) {
+			self::$slim = Slim::getInstance();
+		}
+
+		foreach ($content as $key => $value) {
+			self::$slim->config($key, $value);
+		}
+	}
+
+	protected function parseImports($content, $file)
+	{
+		if (isset($content['imports'])) {
+			$chdir = dirname($file);
+
+			foreach ($content['imports'] as $import) {
+				self::addFile($chdir . DIRECTORY_SEPARATOR . $import['resource'], false);
+			}
+
+			unset($content['imports']);
+		}
+
+		return $content;
+	}
+
+	protected function parseParameters($content)
+	{
+		if (isset($content['parameters'])) {
+			self::$parameters = array_merge(self::$parameters, $content['parameters']);
+
+			unset($content['parameters']);
+		}
+
+		return $content;
 	}
 
 	private function __construct(){}
